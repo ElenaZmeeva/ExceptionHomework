71e524014f0be633da320da5536e1b241f4018dc
Elena <biertli@bk.ru>
2022-06-04 21:17:43 +0300

    HomeWork

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+boolean result = Validator.check("fdaaw", "oiugt5", "oiugt5");
+if (result){
+    System.out.println("Поздравляем!");
+}else {
+    System.out.println("Попробуйте снова");
+}
 
     }
 }
